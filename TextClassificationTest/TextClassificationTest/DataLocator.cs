dc7401942a37cf3d0f4e4903852f6558f2e34ee9
leejamessmith <83655410+leejamessmith@users.noreply.github.com>
2022-11-24 16:12:12 +0000

    Add files via upload

---
+++ TextClassificationTest/TextClassificationTest/DataLocator.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace TextClassificationTest;
+
+public static class DataLocator
+{
+    public static string EnsureDataSetDownloaded(string fileName)
+    {
+
+        // This is the path if the repo has been checked out.
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(),"data", fileName);
+
+        if (!File.Exists(filePath))
+        {
+            // This is the path if the file has already been downloaded.
+            filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+        }
+
+        if (!File.Exists(filePath))
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile($"https://raw.githubusercontent.com/dotnet/csharp-notebooks/main/machine-learning/data/{fileName}", filePath);
+            }
+            Console.WriteLine($"Downloaded {fileName}  to : {filePath}");
+        }
+        else
+        {
+            Console.WriteLine($"{fileName} found here: {filePath}");
+        }
+
+        return filePath;
+    }
+}
---
+++ TextClassificationTest/TextClassificationTest/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.Data.Analysis;
+using Microsoft.ML.TorchSharp;
+using TextClassificationTest;
+
+var mlContext = new MLContext();
+
+
+var yelpReviews = DataLocator.EnsureDataSetDownloaded("yelp_labelled.txt");
+
+var columnNames = new [] {"Text", "Sentiment"};
+
+//var df = DataFrame.LoadCsvFromString(yelp_reviews, separator:'\t',header:false, columnNames:columnNames);
+
+string data = File.ReadAllText(yelpReviews);
+
+DataFrame? df = DataFrame.LoadCsvFromString(data, '\t', false, columnNames);
+
+foreach (var r in df.Head(3).Rows)
+{
+    Console.WriteLine(r[0]);    
+}
+
+DataOperationsCatalog.TrainTestData trainTestSplit = mlContext.Data.TrainTestSplit(df, testFraction:0.2);
+
+var pipeline =
+    mlContext.Transforms.Conversion.MapValueToKey("Label","Sentiment")
+        .Append(mlContext.MulticlassClassification.Trainers.TextClassification(1, sentence1ColumnName: "Text"))
+        .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+var model = pipeline.Fit(trainTestSplit.TrainSet);
+
+var predictionIDV = model.Transform(trainTestSplit.TestSet);
+
+var columnsToSelect = new [] {"Text", "Sentiment", "PredictedLabel"};
+
+var predictions = predictionIDV.ToDataFrame(columnsToSelect);
+
+foreach (var r in predictions.Tail(3).Rows)
+{
+    Console.WriteLine(r[0]);    
+}
